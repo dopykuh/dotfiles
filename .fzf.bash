852495c2ebfefe5bbbd52b8865d7dbe50f86cfcc
Tim Foerster <github@mailserver.1n3t.de>
2017-02-10 22:40:24 +0100

    update quotes
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- .fzf.bash
+++ .fzf.bash
@@ -6,9 +6,9 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source ~/.fzf/shell/completion.bash 2> /dev/null
 
 # Key bindings
 # ------------
-source "~/.fzf/shell/key-bindings.bash"
+source ~/.fzf/shell/key-bindings.bash
 
